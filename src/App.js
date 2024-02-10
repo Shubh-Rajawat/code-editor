983b06a837f37eb276df8dfeed2d3a86410e87a3
Shubh-Rajawat <shubhrajawat9414@gmail.com>
2024-02-10 20:55:11 +0530

    added my portfolio link at the bottom of the page as link name:- creator

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Landing from './components/Landing';
 
 function App() {
   return (
-    <div className=' bg-gray-300' >
+    <div className='relative bg-gray-300' >
       <Landing />
+      <a href='https://shubh-rajawat.vercel.app/'  target='_blank'
+       className=" absolute bottom-2 right-2 cursor-pointer
+      text-gray-900 bg-gray-50 border border-gray-300 focus:outline-none hover:bg-gray-200 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 ">Creator</a>
     </div>
   );
 }
